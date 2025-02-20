26f323a9313744e4b818c438f1e112ff26785848
fazil-mkd <mf645385@gmail.com>
2025-02-20 21:34:39 +0530

    newwwww

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/userSchema');
 require('dotenv').config();
 
-// Google Strategy Configuration
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
