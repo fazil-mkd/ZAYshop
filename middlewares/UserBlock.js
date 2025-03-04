60117866ac8a2242858720b3e692d1e564d661a0
fazil-mkd <mf645385@gmail.com>
2025-03-04 12:14:03 +0530

    new  changes added can work

--- middlewares/UserBlock.js
+++ middlewares/UserBlock.js
@@ -2,50 +2,36 @@ const User = require('../models/userSchema');
 
 const isBlockedMiddleware = async (req, res, next) => {
 
+
   if (req.path.startsWith('/admin')) {
     return next();
   }
 
   try {
 
-
     if (!req.session.user) {
       return next();
     }
 
-    if (req.session.user.isAdmin) {
-      return next();
-    }
-
     const user = await User.findById(req.session.user._id);
-
+    
     if (!user) {
-      req.session.destroy((err) => {
-        if (err) {
-          return res.status(500).render('page-404', { message: 'Error logging out' });
-        }
-        return res.redirect('/');
-      });
-      return;
+      delete req.session.user;
+      return res.redirect('/');
     }
-
 
     if (user.isAdmin) {
       return next();
     }
 
     if (user.isBlocked) {
-      req.session.destroy((err) => {
-        if (err) {
-          return res.status(500).render('page-404', { message: 'Error logging out' });
-        }
-        return res.render('banned', { message: 'You have been blocked by the admin.' });
-      });
-      return;
+      delete req.session.user; 
+      return res.render('banned', { message: 'You have been blocked by the admin.' });
     }
 
     next();
   } catch (error) {
+    
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
